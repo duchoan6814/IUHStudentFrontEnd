329e7d8fd6affcd80cf334aa0c5d5cea6d8907c0
thanhhong593 <thanhhong593@gmail.com>
2021-09-30 21:45:20 +0700

    cm

--- src/components/SinhVien/index.js
+++ src/components/SinhVien/index.js
@@ -252,6 +252,30 @@ const SinhVienComponent = () => {
         visible={visibleModal}
         closeModal={setVisibleModal}
       />
+      <div>
+            <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item name={['user', 'website']} label="Website">
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'introduction']} label="Introduction">
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     </div>
   );
 };
