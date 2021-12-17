eb960b5e4f0d1b8f058ba48b49c70fdebe5d9a6c
Hoan Truong <hoantruong6814@gmail.com>
2021-12-18 00:05:06 +0700

    change enpoit API

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   IS_LOCAL: process.env.NODE_ENV === 'dev',
   IS_PROD: process.env.NODE_ENV === 'prod',
   PORT: 6969,
-  HOST_ADDRESS: "http://18.136.126.228",
+  HOST_ADDRESS: "http://13.212.247.153",
 };
 
 config.GRAPHQL_URL = `${config.HOST_ADDRESS}/graphql`;
